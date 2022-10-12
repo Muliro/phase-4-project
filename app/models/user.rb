8124ed7bee369460017a7a71e64f3b66b1650f9d
muliro <mulirokhaemba@gmail.com>
2022-10-12 16:28:00 +0300

    controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@
 
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true
 
     has_many :reviews
     has_many :movies, through: :reviews
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Movie.create({"name": "bullet train", "lead_actor": "Tom hanks", "director": "Guillermo Del Toro", "movie_poster": "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbullet-train&psig=AOvVaw3EVTmlIaToX_eQu0A4GaqG&ust=1665660961986000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCIjljdzM2voCFQAAAAAdAAAAABAE"})
+
+Review.create({"review": "Awesome movie", "movie_id": 1, "user_id": 1})
+
+Review.create({"review": "highly recommend", "movie_id": 1, "user_id": 1})
+
+User.create({"username": "Michael Muliro", "password_digest": "relapse"})
